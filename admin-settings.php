89b4898137aa61482d6eafe6ca5c2548057dac68
Cole Geissinger <cole@colegeissinger.com>
2013-02-16 12:41:12 -0800

    Initial Commit. Short and sweet.

---
+++ admin-settings.php
@@ -0,0 +1,10 @@
+<?php
+
+	/**
+	 * Plugin Name: WP Admin Settings
+	 * Plugin URI: https://github.com/colegeissinger/wordpress-admin-settings
+	 * Description: A plugin that brings streamlined admin options with no coding knowledge needed.
+	 * Version: 0.1
+	 * Author: Cole Geissinger
+	 * Author URI: http://www.colegeissinger.com
+	 */
